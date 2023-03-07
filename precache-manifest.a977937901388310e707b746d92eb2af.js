f7e333b51f5d17314f3e2c1247d99fadbacc56ba
patrinus-ryan <ryan.santiago.ict@gmail.com>
2023-03-07 17:12:41 +0800

    Updates

---
+++ precache-manifest.a977937901388310e707b746d92eb2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e50c2cb75fa83a2afdca",
+    "url": "/Team-Building/static/css/main.f4a4a813.chunk.css"
+  },
+  {
+    "revision": "e50c2cb75fa83a2afdca",
+    "url": "/Team-Building/static/js/main.e50c2cb7.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/Team-Building/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "9633743f11228b797e7a",
+    "url": "/Team-Building/static/css/2.740e0b0e.chunk.css"
+  },
+  {
+    "revision": "9633743f11228b797e7a",
+    "url": "/Team-Building/static/js/2.9633743f.chunk.js"
+  },
+  {
+    "revision": "a22ba647b48d6b3890fc",
+    "url": "/Team-Building/static/js/runtime~main.a22ba647.js"
+  },
+  {
+    "revision": "ad3b70ce0dc52e79b5629e9dd3bcc9db",
+    "url": "/Team-Building/index.html"
+  }
+];
